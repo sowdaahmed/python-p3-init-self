c50c718ac8c3e937662ef3f1ae7ca162d20ed635
Sowda Ahmed <ayanmoha4949@gmail.com>
2025-06-20 01:49:32 -0700

    done

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,13 @@
+
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name="Guido", age=0):
+        self.name = name
+        self.age = age
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
